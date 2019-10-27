8e33083ed91f9d97f15359f4ccb319165a0da9fa
Pavel Bondar <pavelbondar@Pavels-MacBook-Pro.local>
2019-10-27 13:50:48 +0200

    parse

--- NewsApp/Model/Model.swift
+++ NewsApp/Model/Model.swift
@@ -20,8 +20,29 @@ func loadNews() {
             try? FileManager.default.copyItem(at: urlFile!, to: urlPath)
             
             print(urlPath)
+            
+            parseNews()
+            
+            print(articles.count)
         }
     }
     
     downloadTask.resume()
+}
+
+func parseNews() {
+    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
+    let urlPath = URL(fileURLWithPath: path)
+    
+    let data = try? Data(contentsOf: urlPath)
+    let rootDictionary = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
+    let array = rootDictionary!["articles"] as! [Dictionary<String, Any>]
+    
+    var returnArray: [Article] = []
+    
+    for dict in array {
+        let newArticle = Article(dictionary: dict)
+        returnArray.append(newArticle)
+    }
+    articles = returnArray
 }
