74739747b6c35f409e60810ac5e7a1a10befb1cc
Pavel Bondar <pavelbondar@Pavels-MacBook-Pro.local>
2019-10-27 13:20:49 +0200

    download json

--- NewsApp/Model/Model.swift
+++ NewsApp/Model/Model.swift
@@ -9,7 +9,19 @@
 import Foundation
 
 var articles:[Article] = []
-
+//Your API key is: 84a521a08cf141aa8bbe269df5f99439
 func loadNews() {
+    let url = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-27&sortBy=publishedAt&apiKey=API_KEY")
+    let session = URLSession(configuration: .default)
+    let downloadTask = session.downloadTask(with: url!) { (urlFile, responce, error) in
+        if urlFile != nil {
+            let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
+            let urlPath = URL(fileURLWithPath: path)
+            try? FileManager.default.copyItem(at: urlFile!, to: urlPath)
+            
+            print(urlPath)
+        }
+    }
     
+    downloadTask.resume()
 }
