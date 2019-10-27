16b0186757d64e6696f707cb63cac727f5ce39c0
Pavel Bondar <pavelbondar@Pavels-MacBook-Pro.local>
2019-10-27 13:03:15 +0200

    add model

---
+++ NewsApp/Model/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Pavel Bondar on 10/27/19.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    var author: String
+    var title: String
+    var description: String
+    var url: String
+    var urlToImage: String
+    var publishedAt: String
+    var sourceName: String
+}
---
+++ NewsApp/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  NewsApp
+//
+//  Created by Pavel Bondar on 10/27/19.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import Foundation
+
+var articles:[Article] = []
+
+func loadNews() {
+    
+}
