f275d22e9ae53f774c989f48166d105e573aad20
Pavel Bondar <pavelbondar@Pavels-MacBook-Pro.local>
2019-10-27 12:44:45 +0200

    init

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Pavel Bondar on 10/27/19.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
