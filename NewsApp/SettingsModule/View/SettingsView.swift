50d1579fd2e19edbe4f9bf4cbd7b870f9a1b959c
Pavel Bondar <ivizey@gmail.com>
2020-10-12 16:47:58 +0300

    add settings ui

---
+++ NewsApp/SettingsModule/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Pavel Bondar on 12.10.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIViewController {
+    @IBOutlet private weak var countryPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
